4856b4327ae57040aec4df0b0a92a6f9b2f25130
Stogalosu <dumisteck@gmail.com>
2026-09-07 20:46:19 +0300

    Added file unzip with AdmZip and GPX conversion to GeoJSON with gpxjs.

--- src/app/new-line/actions.ts
+++ src/app/new-line/actions.ts
@@ -1,17 +1,51 @@
 'use server';
 
-import { redirect } from "next/navigation";
 import { addLine } from "@/actions/lines";
+import AdmZip from 'adm-zip';
+import { parseGPXWithCustomParser, GeoJSON } from "@we-gold/gpxjs";
+import { DOMParser } from "xmldom-qsa";
 
 function isValidZipBuffer(buffer: Buffer) {
     return buffer[0] === 0x50 && buffer[1] === 0x4b;
 }
 
-export async function processZip(file: File): Promise<{ data: any, error: any }> {
+export async function processZip(file: File): Promise<{ data: GeoJSON[] | null, error: any }> {
     const buffer = Buffer.from(await file.arrayBuffer());
 
     if(!isValidZipBuffer(buffer))
         return { data: null, error: "Invalid zip file" };
+
+    let zip: AdmZip;
+    try {
+        zip = new AdmZip(buffer);
+    } catch {
+        return { data: null, error: "Could not read zip file" };
+    }
+
+    const gpxEntries = zip.getEntries().filter(e =>
+        e.name.endsWith('.gpx') &&
+        !e.name.startsWith('._') &&
+        !e.entryName.startsWith('__MACOSX/')
+    );
+    if (gpxEntries.length === 0) {
+        return { data: null, error: "No GPX files found in zip" };
+    }
+
+    const customParseMethod = (txt: string): Document | null => {
+        return new DOMParser().parseFromString(txt, "text/xml");
+    }
+
+    let geoJsons = [];
+    for(const entry of gpxEntries) {
+        const gpx = entry.getData().toString('utf-8');
+        const [parsedGpx, error] = parseGPXWithCustomParser(gpx, customParseMethod);
+        if(error)
+            return { data: null, error: `Could not parse GPX file ${entry.name}: ${error}` };
+        const geoJson = parsedGpx.toGeoJSON();
+        geoJsons.push(geoJson);
+    }
+
+    return { data: geoJsons, error: null };
 }
 
 export async function createNewLine(formData: FormData): Promise<{line: Line | null, error: any}> {
@@ -39,7 +73,7 @@ export async function createNewLine(formData: FormData): Promise<{line: Line | n
     const line: Line = { name: name, id: id, type: type, from: from, to: to };
 
     try {
-        addLine(line);
+        await addLine(line);
         return { line: line, error: null };
     } catch (error: any) {
         console.error("Failed to create line: ", error);
