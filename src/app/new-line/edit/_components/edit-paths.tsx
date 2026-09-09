d4190deaf5bb08137bb16539b8d6077a9c371e69
Stogalosu <dumisteck@gmail.com>
2026-09-09 19:23:29 +0300

    Added field to edit end stop as well. Added edit page title.

--- src/app/new-line/edit/_components/edit-paths.tsx
+++ src/app/new-line/edit/_components/edit-paths.tsx
@@ -8,11 +8,13 @@ export default function EditPaths({ stops }: { stops: Stop[] }) {
     const context: NewLineContextType = useNewLineContext();
     const paths = context.data.paths ?? [];
     const startStops = paths.map(p => stops.find(s => s.id == p.startId) ?? stops[0]);
+    const endStops = paths.map(p => stops.find(s => s.id == p.endId) ?? stops[0]);
 
     return (
-        <div className="flex flex-col h-full w-[50vw] items-center overflow-y-auto">
+        <div className="flex flex-col h-full w-[50vw] items-center overflow-y-auto px-12 py-8">
+            <p className="text-2xl font-bold pb-8">Edit paths</p>
             {paths.map((path, i) => (
-                <EditPathCard key={i} stop={startStops[i]} stops={stops}/>
+                <EditPathCard key={i} index={i} startStop={startStops[i]} endStop={endStops[i]} stops={stops}/>
             ))}
         </div>
     );
--- src/components/edit-path-card.tsx
+++ src/components/edit-path-card.tsx
@@ -4,14 +4,17 @@ import LineIcon from "@/components/line-icon";
 import { Button } from "@/components/ui/button";
 
 
-export default function EditPathCard({ stop, stops }: { stop: Stop, stops: Stop[] }) {
+export default function EditPathCard({ index, startStop, endStop, stops }: { index: number, startStop: Stop, endStop: Stop, stops: Stop[] }) {
     const stopsDisplay = stops.map(s => ({ value: `${s.name} (${s.id})`, label: `${s.name} (${s.id})` }));
-    const stopDisplay = { value: `${stop.name} (${stop.id})`, label: `${stop.name} (${stop.id})` };
+    const startDisplay = { value: `${startStop.name} (${startStop.id})`, label: `${startStop.name} (${startStop.id})` };
+    const endDisplay = { value: `${endStop.name} (${endStop.id})`, label: `${endStop.name} (${endStop.id})` };
 
     return (
         <Card>
-            <CardContent className="flex flex-row gap-4">
-                <Combobox options={stopsDisplay} optionName="stop" defaultValue={stopDisplay}></Combobox>
+            <CardContent className="flex flex-row gap-4 items-center">
+                <span>{index + 1}.</span>
+                <Combobox options={stopsDisplay} optionName="stop" defaultValue={startDisplay}/>
+                <Combobox options={stopsDisplay} optionName="stop" defaultValue={endDisplay}/>
             </CardContent>
         </Card>
     );
